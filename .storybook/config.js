d4a50c771c5c478612f3aec4762bc385aa5d20c9
uixcrazy <uixcrazy@gmail.com>
2016-05-27 13:23:01 +0700

    step-01 init

---
+++ .storybook/config.js
@@ -0,0 +1,7 @@
+import { configure } from '@kadira/storybook';
+
+function loadStories() {
+  require('../components/stories/button');
+  // require as many as stories you need.
+}
+configure(loadStories, module);
